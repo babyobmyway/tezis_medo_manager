7fd08820f18a455913475c634ae9a7b462a71b25
m.bobylev <m.bobylev@mininform74.ru>
2023-12-26 14:45:50 +0500

    init

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -3,30 +3,31 @@ import re
 import shutil
 from zipfile import ZipFile
 from ruamel.std.zipfile import delete_from_zip_file
+from config_reader import WORK_DIR, MEDO_DIR
 
 
 def file_direction(catalog_name: str, to_direct: str, to_copy=''):
-    os.mkdir(f'C:/tezis_directory/{to_direct}/{catalog_name}/')
+    os.mkdir(f'{WORK_DIR}/{to_direct}/{catalog_name}/')
     if to_copy != '':
-        os.mkdir(f'C:/tezis_directory/{to_copy}/{catalog_name}/')
-    for _, __, files in os.walk(f'C:/tezis_directory/medo_in/{catalog_name}'):
+        os.mkdir(f'{WORK_DIR}/{to_copy}/{catalog_name}/')
+    for _, __, files in os.walk(f'{WORK_DIR}/{MEDO_DIR}/{catalog_name}'):
         for filename in files:
             if to_copy != '':
-                shutil.copyfile(f'C:/tezis_directory/medo_in/{catalog_name}/{filename}',
-                                f'C:/tezis_directory/{to_copy}/{catalog_name}/{filename}')
-            shutil.move(f'C:/tezis_directory/medo_in/{catalog_name}/{filename}',
-                        f'C:/tezis_directory/{to_direct}/{catalog_name}/{filename}')
-    os.rmdir(f'C:/tezis_directory/medo_in/{catalog_name}/')
+                shutil.copyfile(f'{WORK_DIR}/{MEDO_DIR}/{catalog_name}/{filename}',
+                                f'{WORK_DIR}/{to_copy}/{catalog_name}/{filename}')
+            shutil.move(f'{WORK_DIR}/{MEDO_DIR}/{catalog_name}/{filename}',
+                        f'{WORK_DIR}/{to_direct}/{catalog_name}/{filename}')
+    os.rmdir(f'{WORK_DIR}/{MEDO_DIR}/{catalog_name}/')
     if to_copy != '':
-        return f'C:/tezis_directory/{to_copy}/{catalog_name}'
+        return f'{WORK_DIR}/{to_copy}/{catalog_name}'
 
 
 def copying_files(catalog_name: str, to_direct: str):
-    os.mkdir(f'C:/tezis_directory/{to_direct}/{catalog_name}/')
-    for _, __, files in os.walk(f'C:/tezis_directory/medo_in/{catalog_name}'):
+    os.mkdir(f'{WORK_DIR}/{to_direct}/{catalog_name}/')
+    for _, __, files in os.walk(f'{WORK_DIR}/{MEDO_DIR}/{catalog_name}'):
         for filename in files:
-            shutil.copy(f'C:/tezis_directory/medo_in/{catalog_name}/{filename}',
-                        f'C:/tezis_directory/{to_direct}/{catalog_name}/{filename}')
+            shutil.copy(f'{WORK_DIR}/{MEDO_DIR}/{catalog_name}/{filename}',
+                        f'{WORK_DIR}/{to_direct}/{catalog_name}/{filename}')
 
 
 def remove_file_from_zip(catalog_name: str):
